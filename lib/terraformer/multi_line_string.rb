5c304c0b166b8e81b9b103977cf9188a096c1612
Davy Stevenson <davy.stevenson@gmail.com>
2015-01-31 19:11:48 -0800

    Change how MultiLineStrings are created
    As I'd found when writing tests for all the classes, MultiLineStrings
    are initialized differently than the other classes. All other classes
    can be initialized by the raw coordinates array from their result
    geojson, but not MultiLineString. This also causes some issues when
    trying to validate te structure of MultiLineStrings and the LineStrings
    they create. Not sure exactly why this special logic was in
    MultiLineString, but all other classes use Geometry::Primitive's
    parsing. So if we just do that most things get fixed. Does change how
    things are initialized, as is outlined in the changes to the
    multi_line_string_spec.

--- lib/terraformer/multi_line_string.rb
+++ lib/terraformer/multi_line_string.rb
@@ -3,11 +3,8 @@ module Terraformer
   class MultiLineString < Geometry
 
     def initialize *args
+
       case
-      when Coordinate === args[0] # only one
-        self.coordinates = [Coordinate.from_array(args)]
-      when Array === args[0] # multiple?
-        self.coordinates = Coordinate.from_array args
       when LineString === args[0]
         self.coordinates = args.map &:coordinates
       else
